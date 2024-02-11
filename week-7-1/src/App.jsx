30b86bfdbf3b78a30425a003d1d8c00a39ffb5fb
kav6778 <kavin.iitkgp@gmail.com>
2024-02-12 01:08:14 +0530

    week 7-1 updated

---
+++ week-7-1/src/App.jsx
@@ -0,0 +1,44 @@
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import './App.css'
+const Dashboard = lazy(() => import('./components/Dashboard'))
+const Landing = lazy(() => import('./components/Landing'))
+
+import { Suspense, lazy } from 'react';
+
+function App() {
+
+    //Suspense API
+    return (
+        <div>
+            <BrowserRouter>
+                <Appbar/>
+                <Routes>
+                    <Route path="/dashboard" element = {<Suspense fallback= {"loading..."}>
+                        <Dashboard/></Suspense>}> </Route>
+                    <Route path="/landing" element = {<Suspense fallback= {"loading..."}>
+                        <Landing/></Suspense>}> </Route>
+                </Routes>
+            
+            </BrowserRouter>
+        </div>
+    );
+}
+
+function Appbar(){
+    const navigate = useNavigate();
+    return(
+        <div>
+            <div>
+                <button onClick={()=> {
+                    navigate("/landing")
+                }}>Landing</button>
+                <button onClick={()=> {
+                    navigate("/dashboard")
+                }}>Dashboard</button>
+            </div>
+        </div>
+        //all good dsd
+    )
+}
+export default App;
+
