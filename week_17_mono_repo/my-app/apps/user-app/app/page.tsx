431b47f816705716f2efbf9560ed6e338295248a
kav6778 <kavin.iitkgp@gmail.com>
2024-10-20 11:27:43 +0530

    payym-project-strater-monorepo

---
+++ week_17_mono_repo/my-app/apps/user-app/app/page.tsx
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
+
+export default function Page(): JSX.Element {
+  return (
+    <div className="font-2xl">
+      hi there
+    </div>
+  );
+}
